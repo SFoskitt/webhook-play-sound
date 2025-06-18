bd50dde3b9ee9f1038420ccd245ebce9b01fb9a7
SFoskitt <stephanie.foskitt@gmail.com>
2025-06-18 19:50:06 -0400

    randomize the clip that is played

--- playsound.js
+++ playsound.js
@@ -1,7 +1,9 @@
 module.exports = () => {
     const sound = require('sound-play');
+    const path = require("path");
+
     const options = ['1.mp3','2.mp3','3.mp3','4.mp3','5.mp3','6.mp3','7.mp3','8.mp3'];
     const opt = Math.floor(Math.random() * (options.length - 1 + 1) + 1);
-    console.log("should work");
-    sound.play(options[opt]);
+    const filePath = path.join(__dirname, 'clips', options[opt]);
+    sound.play(filePath);
 };
