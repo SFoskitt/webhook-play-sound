ae40dc089d0b215e48135876d2c42ce27168b02c
SFoskitt <stephanie.foskitt@gmail.com>
2025-06-18 19:57:16 -0400

    refined randomizer to be inclusive

--- playsound.js
+++ playsound.js
@@ -3,7 +3,7 @@ module.exports = () => {
     const path = require("path");
 
     const options = ['1.mp3','2.mp3','3.mp3','4.mp3','5.mp3','6.mp3','7.mp3','8.mp3'];
-    const opt = Math.floor(Math.random() * (options.length - 1 + 1) + 1);
+    const opt = Math.floor(Math.random() * options.length) + 1;
     const filePath = path.join(__dirname, 'clips', options[opt]);
     sound.play(filePath);
 };
